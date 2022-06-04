fd5a6498f074c4917444b7a5c60f51eb3fe173b9
Marius K <marius.into@gmail.com>
2022-06-04 17:51:05 +0300

    show not sheduled todos in upcoming

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import './App.css';
 
 function App() {
   const [ todos, setTodos ] = useState([])
-  const [ status, setStatus ] = useState("Loading, please wait...")
+  const [ view, setView ] = useState(0)
+  const [ status, setStatus ] = useState("Loading, please wait")
   const today = new Date().setDate(new Date().getDate() - 1)
   const tomorrow = new Date().setDate(new Date().getDate() + 1)
-  const overdueTodos = todos.filter( item =>  item.date < today )
-  const todayTodos = todos.filter( item => item.date >= today && item.date < tomorrow )
-  const upcomingTodos = todos.filter( item =>  item.date >= tomorrow )
+  const overdueTodos = todos.filter( item => item.due && new Date(item.due.date) < today )
+  const todayTodos = todos.filter( item => item.due && new Date(item.due.date) >= today && new Date(item.due.date) < tomorrow )
+  const upcomingTodos = todos.filter( item => item.due && new Date(item.due.date) >= tomorrow )
+  const notTodos = todos.filter( item => !item.due )
+
+  const LOADING_VIEW = 0
+  const TODAY_VIEW = 1
+  const NEXT_VIEW = 2
+
     
   function fetchTodoist() {
       let token = localStorage["todoist.token"],
@@ -37,11 +44,11 @@ function App() {
           res.json().then(data => {
               const items = data.items
                   .filter(item => item.responsible_uid === data.user.id || item.project_id === data.user.inbox_project)
-                  .filter(item => item.due)
-                  .map(task => { return { id:task.id, content:task.content, due: task.due, date: new Date(task.due.date) }})
-                  .sort((a, b) => a.due.date > b.due.date ? 1 : -1)
+                  .map(task => { return { id:task.id, content:task.content, due: task.due }})
+                  .sort((a, b) => a.due && b.due && a.due.date > b.due.date ? 1 : -1)
               setTodos(items)
               console.log('items:', items.length)
+              if (view === LOADING_VIEW) setView(TODAY_VIEW)
           })
       })
 
@@ -73,22 +80,31 @@ function App() {
       </div>
       <div className="container">
         <div className="side">
-          <p><i className="material-icons">event</i>Today</p>
-          <p><i className="material-icons">date_range</i>Next 7 days</p>
+          <p onClick={() => setView(TODAY_VIEW)}><i className="material-icons">event</i>Today</p>
+          <p onClick={() => setView(NEXT_VIEW)}><i className="material-icons">date_range</i>Upcoming</p>
           <p><i className="material-icons">calendar_month</i>Calendar</p>
         </div>
         <div className="main">
-        { todos.length === 0 ?
-          <div className='loading'>{ status }</div> :
+        { view === LOADING_VIEW &&  
+          <div className='loading'>{ status }</div> }
+        { view === TODAY_VIEW && 
           <div className="content">
-            <div className="header">Today <span>{ new Date().toLocaleDateString() }</span></div>
-            <div className="list">
-                  <Todolist title={'Overdue'} color={'red'} items={ overdueTodos } />
-                  <Todolist title={`Today - ${new Date().toString().substring(0,15)}`}  color={'green'} items={ todayTodos } />
-                  <Todolist title={'Upcoming'} color={'green'} items={ upcomingTodos } />
+              <div className="header">Today <span>{ new Date().toLocaleDateString() }</span></div>
+              <div className="list">
+                    <Todolist title={'Overdue'} color={'red'} items={ overdueTodos } />
+                    <Todolist title={`Today - ${new Date().toString().substring(0,15)}`}  color={'green'} items={ todayTodos } />
+              </div>
             </div>
-          </div>
-        }            
+        }  
+        { view === NEXT_VIEW && 
+          <div className="content">
+              <div className="header">Upcoming</div>
+              <div className="list">
+                    <Todolist title={'Upcoming tasks'} color={'green'} items={ upcomingTodos } />
+                    <Todolist title={'Not sheduled'} color={'green'} items={ notTodos } />
+              </div>
+            </div>
+        }
         </div>
       </div>
     </div>
--- src/Components/Todolist.js
+++ src/Components/Todolist.js
@@ -3,7 +3,8 @@ import Todo from './Todo'
 export default function Todolist({ title, items, color }) {
     const listview = items.map( todo => {
         const desc = todo.content.split("](")[0].replace("[","")
-        return <Todo key={todo.id} open={()=>openTask(todo.id)} todo={desc} due={todo.date.toLocaleDateString()} color={color} />
+        const due = todo.due ? new Date(todo.due.date).toLocaleDateString() : "not sheduled"
+        return <Todo key={todo.id} open={()=>openTask(todo.id)} todo={desc} due={due} color={color} />
     })
     function openTask(id) {
         console.log('taskId', id)
