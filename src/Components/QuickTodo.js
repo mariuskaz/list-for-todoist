359cf8f7b9f00b08536d6e54c5410dcb56cd0199
Marius K <marius.into@gmail.com>
2022-07-18 17:44:25 +0300

    add buttons to quick todo

--- src/Components/QuickTodo.js
+++ src/Components/QuickTodo.js
@@ -1,40 +1,47 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 export default function QuickTodo({ due, sync }) {
   const [ active, setActive ] = useState(false)
+  const input = useRef()
 
-  function pushTask(event) {
+  function handleInput(event) {
     if (event.key === "Enter") {
-      let task = { content: event.target.value, due_string: due || "" },
-      token = localStorage["todoist.token"] || "none",
-      headers = {
-          'Authorization': 'Bearer ' + token,
-          'Content-Type': 'application/json'
-      }
-
-      fetch('https://api.todoist.com/rest/v1/tasks', { 
-          method: 'POST',
-          headers : headers,
-          body: JSON.stringify(task)
-      })
-
-      .then(response => {
-        sync()
-      })
-
-      setActive(false)
-
-
+      pushTask()
     } else if (event.key === "Escape") {
       setActive(false)
+    } 
+  }
 
+  function pushTask() {
+    let task = { content: input.current.value, due_string: due || "" },
+    token = localStorage["todoist.token"] || "none",
+    headers = {
+        'Authorization': 'Bearer ' + token,
+        'Content-Type': 'application/json'
     }
 
+    fetch('https://api.todoist.com/rest/v1/tasks', { 
+        method: 'POST',
+        headers : headers,
+        body: JSON.stringify(task)
+    })
+
+    .then(response => {
+      sync()
+    })
+
+    setActive(false)
   }
 
   function InputBox() {
-    if (active) return <div><textarea rows="4" autoFocus placeholder="Type text and press Enter" className ="input-box"  onKeyDownCapture={(e) => pushTask(e)} onBlur={() => setActive(false)}  /></div>
-    else return <p className ="commands" onClick={() => {setActive(true)}}><i className="material-icons">add</i>Add task</p>
+    if (active) return (
+      <div>
+        <textarea rows="4" autoFocus placeholder="Type text and press Enter" className ="input-box" onKeyDownCapture={(e) => handleInput(e)} ref={input} />
+        <div className="my-button dark-theme" onClick={pushTask}>Save</div>
+        <div className="my-button" onClick={() => setActive(false)}>Cancel</div>
+      </div>
+    )
+    return <p className ="commands" onClick={() => {setActive(true)}}><i className="material-icons">add</i>Add task</p>
   }
 
   return <InputBox/>
